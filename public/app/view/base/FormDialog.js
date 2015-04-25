6ab83fa6aed5f08852a55b113b002f9995eb56f6
princvaliant <avolos@gmail.com>
2015-04-24 23:34:33 -0700

    Added

---
+++ public/app/view/base/FormDialog.js
@@ -0,0 +1,12 @@
+Ext.define('orf.view.base.FormDialog', {
+  extend: 'Ext.Window',
+  xtype: 'baseformdialog',
+  x: 280,
+  y: 100,
+  closable: true,
+  plain: false,
+  modal: true,
+  layout: 'fit',
+  session: true,
+  items: {}
+});
